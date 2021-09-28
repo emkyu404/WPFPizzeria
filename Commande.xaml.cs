37cd48a690536a4179f9d515d074b381915bdf86
Minh-Quân Bui <contact.buiminhquan@gmail.com>
2021-09-28 14:09:49 +0200

    Ajout boutons mainwindow

--- Commande.xaml.cs
+++ Commande.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,25 +8,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Projet_Pizzaria
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour Commande.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Commande : Window
     {
-        public MainWindow()
+        public Commande()
         {
             InitializeComponent();
-        }
-
-        private void Button_Command_Click(object sender, RoutedEventArgs e)
-        {
-            Commande cmd = new Commande();
-            cmd.Show();
         }
     }
 }
