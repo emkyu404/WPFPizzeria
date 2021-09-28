196146a31dcba573438d24ee0ce73a9e3d7419a2
Minh-Quân Bui <contact.buiminhquan@gmail.com>
2021-09-28 15:12:48 +0200

    ajout de fenêtre Commande xml qui regroupera les fonctionnalités liés aux commandes + ajout de la page Create_Order_Page qui sera lier à la création d'une commande

--- Client.cs
+++ Client.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Projet_Pizzaria
 {
@@ -10,18 +11,40 @@ namespace Projet_Pizzaria
         private string surname;
         private string phoneNumber;
         private DateTime firstOrderDate;
+        private string address;
 
-        public Client(string n, string sn, string pn)
+        private static List<Client> RegisteredClient = new List<Client>();
+
+        public Client(string n, string sn, string pn, string adr)
         {
             this.name = n;
             this.surname = sn;
             this.phoneNumber = pn;
             this.firstOrderDate = DateTime.Now;
+            this.address = adr;
+
+            RegisteredClient.Add(this);
         }
 
         public string getName()
         {
             return name;
+        }
+
+        public string getPhoneNumber()
+        {
+            return phoneNumber;
+        }
+
+        public Client getClientByPhoneNumber(string pn)
+        {
+            Client c = RegisteredClient.FirstOrDefault(cl => cl.getPhoneNumber() == pn);
+            return c;
+        }
+
+        public static List<Client> getRegisteredClient()
+        {
+            return RegisteredClient;
         }
     }
 }
---
+++ Create_Order_Page.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projet_Pizzaria
+{
+    /// <summary>
+    /// Logique d'interaction pour Create_Order_Page.xaml
+    /// </summary>
+    public partial class Create_Order_Page : Page
+    {
+        public Create_Order_Page()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        private void init()
+        {
+            registerClientPnl.Visibility = Visibility.Collapsed;
+            findClientPnl.Visibility = Visibility.Collapsed;
+        }
+
+        private void alreadyRegisteredClientClick(object sender, RoutedEventArgs e)
+        {
+            findClientPnl.Visibility = Visibility.Visible;
+            registerClientPnl.Visibility = Visibility.Collapsed;
+            QuestionBoxPnl.Visibility = Visibility.Collapsed;
+        }
+
+        private void firstClientClick(object sender, RoutedEventArgs e)
+        {
+            registerClientPnl.Visibility = Visibility.Visible;
+            findClientPnl.Visibility = Visibility.Collapsed;
+            QuestionBoxPnl.Visibility = Visibility.Collapsed;
+        }
+    }
+}
