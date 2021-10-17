4211f0da6629b0710b90ccc67ca56fe99dbaac06
Antochg <56629942+Antochg@users.noreply.github.com>
2021-10-17 23:45:22 +0200

    Mise à jour BDD propre

--- App.xaml.cs
+++ App.xaml.cs
@@ -23,14 +23,14 @@ namespace Projet_Pizzaria
         private void Init()
         {
             // initialisation de la liste de clients
-            new Client("Antoine", "azdefzgrethr", "0114564367", new Address(66, "Rue Camille Desmoulins", 6454230, "aachan"));
-            new Client("dfghlhh", "qsdfsgd", "0616846691", new Address(896, "Rue Camille Desmoulins", 54230, "Cachan"));
-            new Client("qsdfgkjhljmk", "sfdghsd", "0547896516846691", new Address(96, "Rue Camille Desmoulins", 14230, "hachan"));
+            new Client("Antoine", "CHENG", "0101010101", new Address(66, "Rue Camille Desmoulins", 94230, "Cachan"));
+            new Client("Minh-Quan", "BUI", "0202020202", new Address(30, "Av. de la République", 94800, "Villejuif"));
+            new Client("Aurélie", "ARNAUD", "0303030303", new Address(143, "Av. de Versailles", 75016, "Paris"));
 
             // initialisation de la liste d'employés
-            new Commis("Antoine", "CHENG", new Address(66, "Rue Camille Desmoulins", 6454230, "Cachan"));
-            new Commis("aezegtrh", "retgrhyte", new Address(98, "Rue Camille Desmoulins", 6454230, "aachan"));
-            new DeliveryMan("thegz", "sfd", new Address(1, "Rue Camille Desmoulins", 6454230, "Bachan"));
+            new Commis("Alex", "HUGO", new Address(89, "Av. Emile Zola", 75015, "Paris"));
+            new Commis("Luc", "BESSON", new Address(20, "Rue du Théâtre", 75015, "Paris"));
+            new DeliveryMan("Jean", "DUJARDIN", new Address(1, "Av. Victor Hugo", 75116, "Paris"));
         }
     }
 }
